10b197f008cb6886fafa4a64d556e7cb47b4781c
Thomas Bunting <thomas1bunting@gmail.com>
2011-12-06 16:48:23 +1100

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	$files = htmlspecialchars_decode(file_get_contents('http://www.ohnorobot.com/index.pl?'.$_SERVER['QUERY_STRING']), ENT_QUOTES);
+	$files = split("<li>", $files);
+	unset($files[0]);
+	foreach($files as $file){
+		echo json_encode(array(/* 'id'   => substr($file, (strpos($file, "comic=")+6), strpos($file, '">') - (strpos($file,  "comic=")+6)),*/
+					'url'  => substr($file, (strpos($file, 'href="')+6), strpos($file, '">') - (strpos($file, 'href="')+6)),
+					'text' => strip_tags(substr($file, (strpos($file, "blockquote>")+11), strpos($file, '<div class="tinylink">') - (strpos($file, "blockquote>")+11))))) . "\n\n\n";
+	}
+?>
